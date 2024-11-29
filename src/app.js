ef565c44f909046714513a69d0e6ac620a23ad0b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-29 11:02:39 -0300

    :sparkles: feat: Require auth end point

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const { sequelize } = require('./models');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(express.json);
+
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 sequelize.sync().then(() => {
